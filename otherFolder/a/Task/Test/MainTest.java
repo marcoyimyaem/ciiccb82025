eeee73ecccb705bfe003743acb408dadc2322b59
marcoyimyaem <marcoyimyaem@gmail.com>
2025-11-08 09:15:29 +0000

    Day 23 Test fixed

--- otherFolder/a/Task/Test/MainTest.java
+++ otherFolder/a/Task/Test/MainTest.java
@@ -1,5 +1,6 @@
 package otherFolder.a.Task.Test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.LocalDate;
 
@@ -9,9 +10,23 @@ import chapter3.DatesAndTimeLesson;
 
 
 public class MainTest {
+LocalDate date4 = LocalDate.of(2025, 7, 1);
     @Test
-    void TestDateOrdinal(){
-        LocalDate date4 = LocalDate.of(2025, 7, 2);
-        assertEquals("2nd of JULY, 2025", DatesAndTimeLesson.dateOrdinalString(date4));
+    void TestDateOrdinalST(){
+        assertTrue(DatesAndTimeLesson.dateOrdinalString(date4).contains("st"), "the message must contains \"st\" suffix ");
+        
+    }
+    @Test
+    void TestDateOrdinalND(){       
+        assertTrue(DatesAndTimeLesson.dateOrdinalString(date4.plusDays(1)).contains("nd"), "the message must contains \"nd\" suffix ");
+    }
+    @Test
+    void TestDateOrdinalRD(){
+        assertTrue(DatesAndTimeLesson.dateOrdinalString(date4.plusDays(2)).contains("rd"), "the message must contains \"rd\" suffix ");
+    }
+    @Test
+    void TestDateOrdinalTH(){
+        assertTrue(DatesAndTimeLesson.dateOrdinalString(date4.plusDays(3)).contains("th"), "the message must contains \"th\" suffix ");
+        assertTrue(DatesAndTimeLesson.dateOrdinalString(date4.plusDays(4)).contains("th"), "the message must contains \"th\" suffix ");
     }
 }
